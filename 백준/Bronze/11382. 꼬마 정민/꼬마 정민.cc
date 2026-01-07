40186b423f21be1ebe46a1a5f9b6dfb191f32d6b
roy11611 <redlion11649@gmail.com>
2026-01-07 19:25:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    long long a, b, c = 0;
+    long long sum = 0;   
+        scanf("%lld %lld %lld",&a ,&b, &c);
+    sum = a + b + c;
+    printf("%lld\n", sum);
+    
+    return 0;
+}
