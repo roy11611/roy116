7662e894573af6138e74dc47a39733c2485b3cd7
roy11611 <redlion11649@gmail.com>
2026-01-18 01:49:30 +0900

    [Bronze V] Title: Multiply, Time: 0 ms, Memory: 1112 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+
+    {
+        int x,y,n,m = 0;
+
+        scanf("%d %d", &x, &y);
+        scanf("%d",&n);
+        scanf("%d",&m);
+
+        printf("%d", n*m);
+
+        return 0;
+    }
